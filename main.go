2d36d2a9b04648f4d3145231fa019767868efcf8
Shanmukha <nandulashanmu123@gmail.com>
2026-06-09 00:58:12 +0530

    Add casino game spin and win logic with random number generation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
 func getName() string {
@@ -31,7 +32,6 @@ func getBet(balance uint) uint {
 	return bet
 }
 
-
 func getSymbolArray(symbols map[string]uint) []string {
 	symbolArray := []string{}
 	for symbol, count := range symbols {
@@ -42,14 +42,65 @@ func getSymbolArray(symbols map[string]uint) []string {
 	return symbolArray
 }
 
+func getRandomNumber(min int, max int) int {
+	randomNumber := min + rand.Intn(max-min+1)
+	return randomNumber
+}
 
 func getSpin(reel []string, rows int, cols int) [][]string {
 	result := [][]string{}
 
 	for i := 0; i < rows; i++ {
 		result = append(result, []string{})
+	}
+	for col := 0; col < cols; col++ {
+		selected := map[int]bool{}
+		for row := 0; row < rows; row++ {
+			for true {
+				randomIndex := getRandomNumber(0, len(reel)-1)
+				_, exists := selected[randomIndex]
+				if !exists {
+					selected[randomIndex] = true
+					result[row] = append(result[row], reel[randomIndex])
+					break
+				}
+			}
+		}
+	}
+	return result
 }
 
+func printSpin(spin [][]string) {
+	for _, row := range spin {
+		for j, symbol := range row {
+			fmt.Printf(symbol)
+			if j != len(row)-1 {
+				fmt.Print(" | ")
+			}
+		}
+		fmt.Println()
+	}
+}
+
+func checkWin(spin [][]string, multipliers map[string]uint) []uint {
+	lines := []uint{}
+	for _, row := range spin {
+		win := true
+		checkSymbol := row[0]
+		for _, symbol := range row[1:] {
+			if checkSymbol != symbol {
+				win = false
+				break
+			}
+		}
+		if win {
+			lines = append(lines, multipliers[checkSymbol])
+		} else {
+			lines = append(lines, 0)
+		}
+	}
+	return lines
+}
 
 func main() {
 	symbols := map[string]uint{
@@ -59,14 +110,14 @@ func main() {
 		"D": 20,
 	}
 
-	// multipliers := map[string]uint{
-	// 	"A": 50,
-	// 	"B": 30,
-	// 	"C": 10,
-	// 	"D": 5,
-	// }
+	multipliers := map[string]uint{
+		"A": 50,
+		"B": 30,
+		"C": 10,
+		"D": 5,
+	}
+
 	symbolArray := getSymbolArray(symbols)
-	fmt.Println(symbolArray)
 
 	balance := uint(300)
 	getName()
@@ -76,6 +127,16 @@ func main() {
 			break
 		}
 		balance -= bet
+		spin := getSpin(symbolArray, 3, 3)
+		printSpin(spin)
+		winningLines := checkWin(spin, multipliers)
+		for i, multi := range winningLines {
+			win := bet * multi
+			balance += win
+			if multi > 0 {
+				fmt.Printf("Won $%d, (%dx) on line #%d\n", win, multi, i+1)
+			}
+		}
 	}
 	fmt.Printf("Your final balance is $%d. Thanks for playing!\n", balance)
 }
